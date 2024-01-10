77a09eb459751edb957b7869d54018b9dedefcdf
0GiS0 <giselatb@outlook.com>
2024-01-10 23:16:17 +0100

    Add Terraform GitHub Actions workflow and update provider and variable configurations

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,9 +1,9 @@
-provider "azurerm"{
-   features{
+provider "azurerm" {
+  features {
     resource_group {
-         prevent_deletion_if_contains_resources = false
+      prevent_deletion_if_contains_resources = false
     }
-   } 
+  }
 }
 
 provider "random" {
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,22 +1,22 @@
-variable env_prefix {
+variable "env_prefix" {
   type        = string
   default     = "dev"
   description = "prefix for all resources"
 }
 
-variable location {
+variable "location" {
   type        = string
   default     = "westeurope"
   description = "location for all resources"
 }
 
-variable sql_administrator_login {
+variable "sql_administrator_login" {
   type        = string
   default     = "sqladminuser"
   description = "SQL administrator login"
 }
 
-variable sql_administrator_login_password {
+variable "sql_administrator_login_password" {
   type        = string
   default     = "H@Sh1CoR3!"
   description = "SQL administrator login password"
