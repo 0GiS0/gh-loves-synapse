1c5cce2ee25252a6f903ae0062b17751ea473927
0GiS0 <giselatb@outlook.com>
2024-01-24 11:00:32 +0100

    Fix synapse workspace ID in spark pool resource

--- terraform/pools.tf
+++ terraform/pools.tf
@@ -1,7 +1,7 @@
 resource "azurerm_synapse_spark_pool" "spark_pool" {
 
     name = "sparkpool"
-    synapse_workspace_id = azurerm_synapse_workspace.synapse_workspace.id
+    synapse_workspace_id = azurerm_synapse_workspace.workspace.id
 
     node_size_family     = "MemoryOptimized"
     node_size            = "Small"
