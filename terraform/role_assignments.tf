c9a7140403e7a375496e0366b0dfc29d6187e940
Gisela <giselatb@outlook.com>
2024-01-25 08:36:44 +0100

    Update role_assignments.tf

--- terraform/role_assignments.tf
+++ terraform/role_assignments.tf
@@ -1,10 +1,10 @@
-resource "azurerm_synapse_role_assignment" "sp_role_assigment" {
-  synapse_workspace_id = azurerm_synapse_workspace.workspace.id
-  role_name            = "Synapse Administrator"
-  principal_id         = data.azurerm_client_config.current.object_id
+#resource "azurerm_synapse_role_assignment" "sp_role_assigment" {
+#  synapse_workspace_id = azurerm_synapse_workspace.workspace.id
+#  role_name            = "Synapse Administrator"
+#  principal_id         = data.azurerm_client_config.current.object_id
 
-  depends_on = [azurerm_synapse_firewall_rule.allow_all]
-}
+#  depends_on = [azurerm_synapse_firewall_rule.allow_all]
+#}
 
 resource "azurerm_synapse_role_assignment" "user_role_assigment" {
     synapse_workspace_id = azurerm_synapse_workspace.workspace.id
